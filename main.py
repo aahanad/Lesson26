cf568bcb4b84776d4f90c8a1bf7dd5457e94ffbc
aahanad <150878761+aahanad@users.noreply.github.com>
2024-11-19 18:32:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ numbers.place(x=370,y=75)
 numbers["values"]=tuple(range(101))
 option=IntVar()
 radio10=Radiobutton(screen,text="10",variable=option,value=10)
-radio10.place(x=410,y=75)
+radio10.place(x=430,y=75)
 radio20=Radiobutton(screen,text="20",variable=option,value=20)
-radio20.place(x=410,y=100)
+radio20.place(x=430,y=100)
 radio30=Radiobutton(screen,text="30",variable=option,value=30)
-radio30.place(x=410,y=120)
+radio30.place(x=430,y=120)
+def display_table():
+    tables=""
+    for i in range(option.get()+1):
+        tables+=str(i)+"X"+str(Number.get())+"="+str(Number.get()*i)+"\n"
+    table.configure(text=tables)
+display=Button(screen,text="generate table",command=display_table)
+display.place(x=370,y=145)
+table=Label(screen,font=("Brush Script Std",13))
+table.place(x=370,y=160)
 screen.mainloop()
